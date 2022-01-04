e8d294e8001925ff5d33d0d25529c24d8a675471
Egor Golikov <20020110ll@gmail.com>
2022-01-04 13:45:13 +0700

    При скрытии сеанса (из-за пройденного времени) - происходит сдвиг предлагаемых сеансов. Отредакттирован выбор времени сеанса

--- Menu.cpp
+++ Menu.cpp
@@ -207,6 +207,18 @@ void Menu::Description(Order& order)
 	}
 	else
 	{
+		int h = 0;
+		for (int i = 0; i < 3; i++)
+		{
+			if (cinema->DeConvert_Time(cinema->films[order.film - 1].time[(order.day - 1) * 3 + i]) > cinema->DeConvert_Time(Time::RetTime(0)))
+			{
+				h++;
+			}
+		}
+		if (h != 3)
+		{
+			order.time = order.time + (3 -  h);
+		}
 		cout << "\n\n   A B C D E F G H I J";
 		for (int i = 0; i < 10; i++)
 		{
